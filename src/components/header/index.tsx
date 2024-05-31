bd6dff913c34dac510629e1ede4b32a7cdfd80c7
HerbertKayky <herbertkayky9@gmail.com>
2024-05-31 19:21:44 -0300

    criando e estilizando header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
+
+export function Header() {
+  const [top, setTop] = useState(true);
+
+  const scrollHander = () => {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHander);
+
+    return () => window.removeEventListener("scroll", scrollHander);
+  }, [top]);
+
+  return (
+    <header
+      className={`${styles.header} ${!top ? styles.fixed : styles.background}`}
+    >
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contentLogo}>
+            <Link href="/">Dev Motors</Link>
+          </div>
+
+          <nav className={styles.nav}>
+            <Link href="/">Home</Link>
+            <Link href="/#servicos">Serviços</Link>
+            <Link href="/#contatos">Contatos</Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
