f5be424590a917420ebcd3f5ef93f4c0af94f5a2
HerbertKayky <herbertkayky9@gmail.com>
2024-06-04 00:38:09 -0300

    criando 'sobre' e 'serviços' da página

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Container } from "@/components/container";
 import { Hero } from "@/components/hero";
+import { Services } from "@/components/home/services";
 import { Submenu } from "@/components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/home.type";
@@ -17,6 +19,11 @@ export default async function Home() {
         bannerUrl={object.metadata.banner.url}
         icon={<Phone size={24} color="#FFF" />}
       />
+
+      <Container>
+        <Services object={object} />
+      </Container>
+      
     </main>
   );
 }
